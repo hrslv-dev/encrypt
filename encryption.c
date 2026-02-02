7eb1b43025a8ffb7131616d0fc1917fd9ce73cf8
hrslv-dev <henriqueaugustoss17@gmail.com>
2026-02-02 20:23:21 -0300

    adding functions

--- encryption.c
+++ encryption.c
@@ -1,7 +1,25 @@
 # include <stdio.h>
+# include <cs50.h> 
+# include <stdlib.h>
+# include <time.h>
 
-int generate_public_key()
+const char alphabet[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+// Change for array of chars
+string encrypt (string plaintext, int key); 
+string decrypt(string cipher, int key); 
+string generate_key(void);
 
 int main(void){
     
+}
+// CIFRA VIGENERE
+string generate_key(void){ 
+    srand(time(NULL)); 
+    // for com numero aleatorio gerado entre 5 a 10  
+    
+}
+string encrypt(string plaintext, int key){ 
+    // Use a data structure that stores numbers and chars respectivaly 
+    // Probrably is a hashmap that i should use 
+    // DATA STRUCUTURES and ALGORITMS ( memory ? )
 }
