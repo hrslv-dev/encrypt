f4a72cf48a28b6365772442ea05f01f83405f6e3
hrslv-dev <henriqueaugustoss17@gmail.com>
2026-02-05 19:05:17 -0300

    func encrypt

--- encryption.c
+++ encryption.c
@@ -4,19 +4,21 @@
 # include <time.h>
 
 const char alphabet[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-// Change for array of chars
 string encrypt (string plaintext, int key); 
 string decrypt(string cipher, int key); 
 string generate_key(void);
 
 int main(void){
-    
+    string plaintext = get_string("Plaintext : ");
+    string key = generate_key(); 
+    encrypt(plaintext, key); 
 }
 // CIFRA VIGENERE
 string generate_key(void){ 
-    // RANDOM NUMBER FOR FOR LOOP 
-    srand(time(NULL));  // max = 5 min = 2
-    // UNDERSTAND THIS FUNC HERE BETTER 
+    // srand chooses what sequence rand will follow because rand is not random at all 
+    // it needs a seed to start the sequence of possibilities to ranh().
+    srand(time(NULL));// here it receives an unsigned int or a positive integer 
+    // rand() is not random.
     int random = rand() % (10 - 5 + 1) + 5;
     int rand_char;
     string key;  
@@ -24,10 +26,25 @@ string generate_key(void){
         rand_char = rand()% ( 25 - 0 + 1) + 0; 
         key[i] = alphabet[rand_char]; 
     }
-    
+    printf("%s\n", key); 
+    return key; 
 }
+
+// plaintext is an input . 
 string encrypt(string plaintext, int key){ 
-    // Use a data structure that stores numbers and chars respectivaly 
-    // Probrably is a hashmap that i should use 
-    // DATA STRUCUTURES and ALGORITMS ( memory ? )
+
+    // conditional if the plaintext is bigger then the key: 
+    string new_key; 
+    while(strlen(plaintext) > strlen(key)){ 
+        // need that variable to change the length; 
+        // Here needs "malloc" and "realloc" functions. 
+        // why? to aloc the memory to get the string bigger. 
+        key = 
+    }
+
+    string cipher; // this function will return this value. 
+    for(int i = 0, n = strlen(plaintext); i < n; i++){ 
+        cipher = alphabet[plaintext[i] + key[i]]; 
+        
+    }            
 }
